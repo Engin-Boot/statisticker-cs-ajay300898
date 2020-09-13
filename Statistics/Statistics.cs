7394f57c0ab45a031e3a6a88d1fbfc4f2c6b0eff
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-13 22:46:59 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -7,14 +7,19 @@ namespace Statistics
         private float average{ get; set;}
         private float max{ get; set;}
         private float min{ get; set;}
+        public Stats(float _avg, float _max, float _min){
+            average = _avg;
+            max = _max;
+            min = _min;
+        }
     }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
             if(numbers.Count > 0){
-                return new Stats(){average = numbers.Average(), max = numbers.Max(), min = numbers.Min() };
+                return new Stats(numbers.Average(), numbers.Max(), numbers.Min());
             }
-            return new Stats(){average = float.NaN, max = float.NaN, min = float.NaN };
+            return new Stats(float.NaN, float.NaN, float.NaN);
         }
     }
 }
